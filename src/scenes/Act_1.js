55c744e1ae6faf0d6b6c2a9c5d3736a317717c68
luoliang01 <luoliang01@58ganji.com>
2017-12-05 16:38:04 +0800

    add scene container

--- src/scenes/Act_1.js
+++ src/scenes/Act_1.js
@@ -7,8 +7,7 @@ import {
 const Act = new Container()
 // 挂载一个初始化场景方法
 Act.init = () => {
-  const sprite = new Sprite(resources['assets/cat.png'].texture)
-  Act.addChild(sprite)
+  
 }
 
 export default Act
--- src/scenes/Act_2.js
+++ src/scenes/Act_2.js
@@ -7,10 +7,6 @@ import {
 const Act = new Container()
 
 Act.init = () => {
-  const sprite = new Sprite(resources['assets/cat.png'].texture)
-  sprite.x = 50;
-  sprite.y = 50;
-  Act.addChild(sprite)
 }
 
 export default Act
