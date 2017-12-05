9d1f12174268015629d3208f89c855d6ccc2a975
luoliang01 <luoliang01@58ganji.com>
2017-12-05 17:33:53 +0800

    add Container

--- src/scenes/Act_1.js
+++ src/scenes/Act_1.js
@@ -7,7 +7,20 @@ import {
 const Act = new Container()
 // 挂载一个初始化场景方法
 Act.init = () => {
-  
+  const buttonLeft = new Sprite(resources['assets/cat.png'].texture)
+  const buttonRight = new Sprite(resources['assets/cat.png'].texture)
+
+  buttonLeft.buttonMode = true;
+  buttonLeft.interactive = true;
+  buttonLeft.x = 50;
+  buttonLeft.y = 50;
+
+  buttonRight.buttonMode = true;
+  buttonRight.interactive = true;
+  buttonRight.x = 100;
+  buttonRight.y = 100
+
+  Act.addChild(buttonLeft, buttonRight)
 }
 
 export default Act
